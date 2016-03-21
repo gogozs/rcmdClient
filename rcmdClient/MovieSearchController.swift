34f14de353e5eee04b45e4b15e6d71ddc2965f45
Song Zhou <zhousong1993@gmail.com>
2016-03-21 21:53:43 +0800

    add new files

---
+++ rcmdClient/MovieSearchController.swift
@@ -0,0 +1,13 @@
+//
+//  MovieSearchController.swift
+//  rcmdClient
+//
+//  Created by Song Zhou on 3/7/16.
+//  Copyright © 2016 Song Zhou. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchController: UISearchController {
+    
+}
---
+++ rcmdClient/RCMDTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  RCMDTabBarController.swift
+//  rcmdClient
+//
+//  Created by Song Zhou on 3/21/16.
+//  Copyright © 2016 Song Zhou. All rights reserved.
+//
+
+import UIKit
+
+class RCMDTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
